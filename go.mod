1e1ded7a614642c22431c590ba1b747780c5ecbd
Niklas Bonkowski <github.daylong239@slmails.com>
2026-07-12 00:35:09 +0200

    feat: implement a concurrent read/print from a given file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nikbonk/httpfromtcp
+
+go 1.27rc1
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const inputFilePath = "messages.txt"
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+
+		currentLineContents := ""
+		for {
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+					currentLineContents = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- currentLineContents + parts[i]
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+
+	}()
+	return lines
+}
+
+func main() {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+	defer f.Close()
+
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+
+}

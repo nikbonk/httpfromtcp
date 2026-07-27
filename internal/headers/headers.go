5dfc07b59c1b6e8778c488040d23339dba892d4e
Niklas Bonkowski <github.daylong239@slmails.com>
2026-07-27 19:47:49 +0200

    feat: no clue man...

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -2,61 +2,46 @@ package headers
 
 import (
 	"bytes"
-	"errors"
+	"fmt"
+	"slices"
 	"strings"
-	"unicode"
 )
+
+const crlf = "\r\n"
 
 type Headers map[string]string
 
-const (
-	clrf                = "\r\n"
-	allowedSpecialChars = "!#$%&'*+-.^_`|~"
-)
-
 func NewHeaders() Headers {
-	return make(Headers)
+	return map[string]string{}
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	idx := bytes.Index(data, []byte(clrf))
+	// print the data with crlf encoding
+
+	idx := bytes.Index(data, []byte(crlf))
 	if idx == -1 {
 		return 0, false, nil
 	}
 	if idx == 0 {
-		return idx + len(clrf), true, nil
-	}
-	headerText := data[:idx]
-	normalizedHeaderText := strings.TrimLeftFunc(string(headerText), unicode.IsSpace)
-	if len(normalizedHeaderText) < len(headerText) {
-		return 0, false, errors.New("header text starts with space")
-	}
-	normalizedHeaderText = strings.TrimFunc(normalizedHeaderText, unicode.IsSpace)
-	headerParts := strings.SplitN(normalizedHeaderText, ":", 2)
-	if len(headerParts) != 2 {
-		return 0, false, errors.New("invalid header format")
-	}
-	headerName := strings.TrimSpace(headerParts[0])
-	if len(headerName) < len(headerParts[0]) {
-		return 0, false, errors.New("header name includes spaces")
-	}
-	headerName = strings.ToLower(headerName)
-	if !isAllowedString(headerName) {
-		return 0, false, errors.New("header name contains invalid characters")
+		// the empty line
+		// headers are done, consume the CRLF
+		return 2, true, nil
 	}
 
-	headerValue := strings.TrimSpace(headerParts[1])
-	h.Set(headerName, headerValue)
-	return idx + len(clrf), false, nil
-}
-func (h Headers) Set(name, value string) {
-	_, ok := h[name]
-	if ok {
-		existingValue := h[name]
-		h[name] = existingValue + ", " + value
-		return
+	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+	key := strings.ToLower(string(parts[0]))
+
+	if strings.ContainsAny(key, " \t") {
+		return 0, false, fmt.Errorf("invalid header name: %s", key)
 	}
-	h[name] = value
+
+	value := bytes.TrimSpace(parts[1])
+	key = strings.TrimSpace(key)
+	if !validTokens([]byte(key)) {
+		return 0, false, fmt.Errorf("invalid header token found: %s", key)
+	}
+	h.Set(key, string(value))
+	return idx + 2, false, nil
 }
 
 func (h Headers) Get(key string) (string, bool) {
@@ -65,12 +50,42 @@ func (h Headers) Get(key string) (string, bool) {
 	return v, ok
 }
 
-func isAllowedString(header string) bool {
-	for _, char := range header {
-		if ('a' <= char && char <= 'z') || ('A' <= char && char <= 'Z') || ('0' <= char && char <= '9') || strings.ContainsRune(allowedSpecialChars, char) {
-			continue
+func (h Headers) Set(key, value string) {
+	key = strings.ToLower(key)
+	v, ok := h[key]
+	if ok {
+		value = strings.Join([]string{
+			v,
+			value,
+		}, ", ")
+	}
+	h[key] = value
+}
+
+func (h Headers) Override(key, value string) {
+	key = strings.ToLower(key)
+	h[key] = value
+}
+
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+
+// validTokens checks if the data contains only valid tokens
+// or characters that are allowed in a token
+func validTokens(data []byte) bool {
+	for _, c := range data {
+		if !isTokenChar(c) {
+			return false
 		}
-		return false
 	}
 	return true
+}
+
+func isTokenChar(c byte) bool {
+	if c >= 'A' && c <= 'Z' ||
+		c >= 'a' && c <= 'z' ||
+		c >= '0' && c <= '9' {
+		return true
+	}
+
+	return slices.Contains(tokenChars, c)
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,8 @@
 package server
 
 import (
-	"bytes"
 	"fmt"
-	"io"
+	"log"
 	"net"
 	"sync/atomic"
 
@@ -11,20 +10,7 @@ import (
 	"github.com/nikbonk/httpfromtcp/internal/response"
 )
 
-type Handler func(w io.Writer, req *request.Request) *HandlerError
-
-type HandlerError struct {
-	StatusCode response.StatusCode
-	Message    string
-}
-
-func (he HandlerError) Write(w io.Writer) {
-	response.WriteStatusLine(w, he.StatusCode)
-	messageBytes := []byte(he.Message)
-	headers := response.GetDefaultHeaders(len(messageBytes))
-	response.WriteHeaders(w, headers)
-	w.Write(messageBytes)
-}
+type Handler func(w *response.Writer, req *request.Request)
 
 // Server is an HTTP 1.1 server
 type Server struct {
@@ -47,7 +33,8 @@ func Serve(port int, handler Handler) (*Server, error) {
 }
 
 func (s *Server) Close() error {
-	if s.closed.CompareAndSwap(false, true) {
+	s.closed.Store(true)
+	if s.listener != nil {
 		return s.listener.Close()
 	}
 	return nil
@@ -57,7 +44,11 @@ func (s *Server) listen() {
 	for {
 		conn, err := s.listener.Accept()
 		if err != nil {
-			return
+			if s.closed.Load() {
+				return
+			}
+			log.Printf("Error accepting connection: %v", err)
+			continue
 		}
 		go s.handle(conn)
 	}
@@ -65,25 +56,15 @@ func (s *Server) listen() {
 
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
+	w := response.NewWriter(conn)
 	req, err := request.RequestFromReader(conn)
 	if err != nil {
-		hErr := &HandlerError{
-			StatusCode: response.StatusCodeBadRequest,
-			Message:    err.Error(),
-		}
-		hErr.Write(conn)
+		w.WriteStatusLine(response.StatusCodeBadRequest)
+		body := []byte(fmt.Sprintf("Error parsing request: %v", err))
+		w.WriteHeaders(response.GetDefaultHeaders(len(body)))
+		w.WriteBody(body)
 		return
 	}
-	buf := bytes.NewBuffer([]byte{})
-	hErr := s.handler(buf, req)
-	if hErr != nil {
-		hErr.Write(conn)
-		return
-	}
-	b := buf.Bytes()
-	response.WriteStatusLine(conn, response.StatusCodeSuccess)
-	headers := response.GetDefaultHeaders(len(b))
-	response.WriteHeaders(conn, headers)
-	conn.Write(b)
+	s.handler(w, req)
 	return
 }
